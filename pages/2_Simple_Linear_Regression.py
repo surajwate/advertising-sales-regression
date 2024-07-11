8a29d80bdf0d59af9bbd80b082709bf5f4a4c690
Suraj Wate <suraj.wate@gmail.com>
2024-07-12 00:10:20 +0100

    Improve metrics display

--- pages/2_Simple_Linear_Regression.py
+++ pages/2_Simple_Linear_Regression.py
@@ -13,44 +13,66 @@ st.write(
 
     We have created three models for three different advertising mediums using 80% of the data.
     """
-    )
+)
 
 ad_medium = st.selectbox("Select Advertising Medium", ["TV", "Radio", "Newspaper"])
 
-# Load the model
-model = joblib.load(f"models/simple_linear_regression_{ad_medium}.pkl")
 
-# Performance of the model
+# Function to load the model and metrics
+def load_model_and_metrics(ad_medium):
+    model = joblib.load(f"models/simple_linear_regression_{ad_medium}.pkl")
+    metrics = pd.read_csv(f"models/metrics_simple_linear_regression_{ad_medium}.csv")
+    return model, metrics
+
+
+# Load the model and metrics
+model, metrics = load_model_and_metrics(ad_medium)
+
+# Performance metrics of the selected model
 st.subheader(f"Performance of the model for {ad_medium} Advertisement")
-metrics = pd.read_csv(f"models/metrics_simple_linear_regression_{ad_medium}.csv")
-st.write(metrics)
+mse = metrics["MSE"][0]
+r2 = metrics["R2"][0]
+
+metrics_dict = {
+    "Metric": ["Mean Squared Error", "R-squared (R2) Score"],
+    "Value": [mse, r2]
+    }
+st.dataframe(pd.DataFrame(metrics_dict, index=[1, 2]))
 
 st.subheader(f"Predict Sales based on {ad_medium} Advertisement")
 
+
 # Function to predict sales
-def predict_sales(budget):
+def predict_sales(model, budget):
     input_data = pd.DataFrame({ad_medium: [budget]})
     # Predict sales
     sales = model.predict(input_data)
     return sales[0]
 
+
 # Collect input data from the user
 if ad_medium == "TV":
-    max_value=300.0
-    value=150.0
-    step=1.0
+    max_value = 300.0
+    value = 150.0
+    step = 1.0
 elif ad_medium == "Radio":
-    max_value=50.0
-    value=25.0
-    step=0.5
+    max_value = 50.0
+    value = 25.0
+    step = 0.5
 else:
-    max_value=120.0
-    value=30.0
-    step=1.0
+    max_value = 120.0
+    value = 30.0
+    step = 1.0
 
-user_budget = st.number_input(f"Enter Budget for {ad_medium} advertisement in 1000$ (between 0 and {max_value}):", min_value=0.0, max_value=max_value, value=value, step=step)
+user_budget = st.number_input(
+    f"Enter Budget for {ad_medium} advertisement in 1000$ (between 0 and {max_value}):",
+    min_value=0.0,
+    max_value=max_value,
+    value=value,
+    step=step,
+)
 
 if st.button("Predict Sales"):
     # Predict sales
-    sales = predict_sales(user_budget)
+    sales = predict_sales(model, user_budget)
     st.write(f"Predicted Sales: {sales:.2f} M$")
